689d728f665a84df124ba00b86fd87928f725793
Uri_Klevcov <a-08-137.mail.ru>
2023-01-20 01:19:37 +0600

    finish

--- Task1.py
+++ Task1.py
@@ -6,13 +6,26 @@ file = 'direct.txt'
 
 
 while True:
-    number = t.Menu()
-    if not number in range(1, 8):
+    number = t.Menu('start')
+    if not number in range(1, 7):
         os.system('cls')
         print('Такого номера в меню не представлено. Перезапустите программу и попробуйте еще раз.\n')
         break
     elif number == 1:
         t.PrintFile(file)
+    elif number == 2:
+        t.FindName(file)
+        number = t.Menu('person')
+    elif number == 3:
+        t.AddPerson(file)
+    elif number == 4:
+        t.DelPerson(file)
+    elif number == 5:
+        t.CorrectTel(file)
+    else: break
+        
+    
+    
         
     
 
